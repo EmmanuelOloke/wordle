10e70c6c9760eb427896adaa54c29a286087f97e
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-01 02:21:58 +0100

    generate game letter boxes and rows

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ let nextLetter = 0;
 let rightGuess = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 console.log(rightGuess);
+
+const initializeBoard = () => {
+  let board = document.getElementById('game-board');
+
+  for (let i = 0; i < GUESES; i++) {
+    let row = document.createElement('div');
+    row.className = 'letter-row';
+
+    for (let j = 0; j < 5; j++) {
+      let box = document.createElement('div');
+      box.className = 'letter-box';
+      row.appendChild(box);
+    }
+    board.appendChild(row);
+  }
+};
+
+initializeBoard();
